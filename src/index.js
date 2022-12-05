dbc4f6b3a0cb177bb4e71033d19f39573625a846
Micah Havens <micah.apex@gmail.com>
2022-12-05 13:19:45 -0500

    Start work on server-to-server protocol
    This is completely untested. Do not use this. It needs to be pushed
    so we can move our implementation of Omnichat and our documentation
    along.
    
    This commit begins the implementation of the Omni server-to-Omni
    server communication protocol, which takes place over Websockets.

--- src/index.js
+++ src/index.js
@@ -1,11 +1,14 @@
 const fs = require("fs");
 const { v4: uuidv4 } = require("uuid");
 const { MemorySource } = require("@orbit/memory");
+const { WebSocketServer } = require("ws");
 //const { JSONAPISource } = require("@orbit/jsonapi");
 
 const schema = require("./schema");
 const memory = new MemorySource({ schema });
 
+let wss = null;
+let peer_connections = {};
 let online_users = {};
 let event_handlers = {};
 
@@ -22,44 +25,6 @@ class EventHandler {
 		this.callback(data);
 	}
 }
-
-/*return {
-	type: "channel",
-	id: this.id,
-	attributes: {
-		name: this.name,
-		admin_only: this.admin_only,
-		is_private: this.is_private
-	},
-	relationships: {
-		peer: { data: { type: "peer", id: this.origin } }
-	}
-};*/
-
-/*return {
-	type: "user",
-	id: this.id,
-	attributes: {
-		username: this.username,
-		admin: this.admin
-	},
-	relationships: {
-		peer: { data: { type: "peer", id: this.origin } }
-	}
-};*/
-
-/*return {
-	type: "message",
-	id: this.id,
-	attributes: {
-		content: this.content,
-		timestamp: this.timestamp
-	},
-	relationships: {
-		channel: { data: { type: "channel", id: this.channel_id } },
-		user: { data: { type: "user", id: this.user_id } }
-	}
-};*/
 
 function load_server_or_make_default(id, name) {
 	this_server = memory.cache.query((q) => q.findRecord({ type: "peer", id }));
@@ -127,9 +92,13 @@ function emit(event, data) {
 	});
 }
 
-function create_user(username, admin = false) {
+function create_user(username, admin = false, peer_id = null) {
 	if (find_user_by_username(username) != null) {
 		return null;
+	}
+
+	if (peer_id == null) {
+		peer_id = this_server.id;
 	}
 
 	const id = uuidv4();
@@ -139,7 +108,7 @@ function create_user(username, admin = false) {
 		id,
 		attributes: { username, admin },
 		relationships: {
-			peer: { data: { type: "peer", id: this_server.id } }
+			peer: { data: { type: "peer", id: peer_id } }
 		}
 	};
 
@@ -264,6 +233,23 @@ function create_channel(name, admin_only = false, is_private = false) {
 	return channel.id;
 }
 
+function create_remote_channel(name, channel_id, peer_id) {
+	let channel = {
+		type: "channel",
+		id: channel_id,
+		attributes: {
+			name
+		},
+		relationships: {
+			peer: { data: { type: "peer", id: peer_id } }
+		}
+	}
+
+	memory.update((t) => t.addRecord(channel));
+	emit("channel_create", channel);
+	return channel.id;
+}
+
 function delete_channel(id) {
 	let channel = get_channel(id);
 
@@ -328,9 +314,234 @@ function get_messages(channel_id, timestamp, limit = 50) {
 	).sort((a, b) => a.attributes.timestamp - b.attributes.timestamp);
 }
 
+function peer_online(peer_id) {
+	if (!get_peer(peer_id)) {
+		return false;
+	}
+
+	online_peers[peer_id] = true;
+	emit("peer_online", peer_id);
+	return true;
+}
+
+function set_up_handlers(websocket, peer_id) {
+	websocket.on("message", (message) => {
+		let data = JSON.parse(message);
+
+		switch (data.type) {
+			case "login":
+				// Should be fine?
+				if (login_user(data.id)) {
+					websocket.send(JSON.stringify({
+						type: "login_success",
+						id: data.id
+					}));
+				} else {
+					websocket.send(JSON.stringify({
+						type: "login_failure",
+						id: data.id
+					}));
+				}
+				break;
+			case "logout":
+				// Should be fine?
+				if (logout_user(data.id)) {
+					websocket.send(JSON.stringify({
+						type: "logout_success",
+						id: data.id
+					}));
+				} else {
+					websocket.send(JSON.stringify({
+						type: "logout_failure",
+						id: data.id
+					}));
+				}
+				break;
+			case "create_user":
+				//let new_user_id = create_user(data.name, false, data);
+				// TODO: Add peer's user to db
+				websocket.send(JSON.stringify({
+					type: "create_user_success",
+					id: new_user_id
+				}));
+				break;
+			case "delete_user":
+				// This should be fine?
+				if (delete_user(data.id)) {
+					websocket.send(JSON.stringify({
+						type: "delete_user_success",
+						id: data.id
+					}));
+				} else {
+					websocket.send(JSON.stringify({
+						type: "delete_user_failure",
+						id: data.id
+					}));
+				}
+				break;
+			case "create_channel":
+				let new_channel_id = create_remote_channel(data.name, data.id, peer_id);
+				websocket.send(JSON.stringify({
+					type: "create_channel_success",
+					id: new_channel_id
+				}));
+				break;
+			case "delete_channel":
+				if (delete_channel(data.id)) {
+					websocket.send(JSON.stringify({
+						type: "delete_channel_success",
+						id: data.id
+					}));
+				} else {
+					websocket.send(JSON.stringify({
+						type: "delete_channel_failure",
+						id: data.id
+					}));
+				}
+				break;
+			case "send_message":
+				let new_message_id = send_message(data.user_id, data.channel_id, data.content);
+				websocket.send(JSON.stringify({
+					type: "send_message_success",
+					id: new_message_id
+				}));
+				break;
+			case "delete_message":
+				if (delete_message(data.id)) {
+					websocket.send(JSON.stringify({
+						type: "delete_message_success",
+						id: data.id
+					}));
+				} else {
+					websocket.send(JSON.stringify({
+						type: "delete_message_failure",
+						id: data.id
+					}));
+				}
+				break;
+			case "get_messages":
+				let messages = get_messages(data.channel_id, data.timestamp, data.limit);
+				websocket.send(JSON.stringify({
+					type: "get_messages_success",
+					messages: messages
+				}));
+				break;
+			default:
+				console.log("Unknown message type: " + data.type);
+		}
+	});
+}
+
+function send_pair_request(ip, port) {
+	let socket = new WebSocket("ws://" + ip + ":" + port);
+
+	socket.on("open", () => {
+		socket.send(JSON.stringify({
+			type: "pair_request",
+			id: peer_id,
+			name: this_server.attributes.name,
+			address: this_server.attributes.address,
+			port: this_server.attributes.port
+		}));
+	});
+
+	socket.on("message", (message) => {
+		let data = JSON.parse(message);
+
+		switch (data.type) {
+			case "pair_accept":
+				let peer = {
+					type: "peer",
+					id: data.id,
+					attributes: {
+						name: data.name,
+						address: data.address,
+						port: data.port
+					},
+					relationships: {
+						channels: { data: [] },
+						users: { data: [] }
+					}
+				}
+
+				// Update orbit db
+				memory.update((t) => {
+					t.addRecord(peer);
+				});
+				
+				break;
+			case "pair_reject":
+				console.log(`Pairing rejected by ${ip}:${port}`);
+				break;
+			default:
+				console.log(`Unknown response to pair request: ${data.type}`);
+		}
+	});
+}
+
+function connect_to_peer(peer) {
+	if (peer.id == this_server.id) {
+		return false;
+	}
+
+	if (connected_peers[peer.id]) {
+		return false;
+	}
+
+	let socket = new WebSocket(`ws://${peer.ip}:${peer.port}/`);
+
+	socket.on("open", () => {
+		socket.send(JSON.stringify({
+			type: "handshake",
+			id: this_server.id
+		}));
+	});
+
+	connected_peers[peer.id] = socket;
+	return true;
+}
+
+function await_handshake(socket) {
+	return new Promise((resolve, reject) => {
+		socket.on("message", (message) => {
+			let data = JSON.parse(message);
+
+			if (data.type == "handshake") {
+				resolve(data);
+			} else {
+				reject("Invalid handshake");
+			}
+		});
+	});
+}
+
 function start(config_path) {
 	config = JSON.parse(fs.readFileSync(config_path, "utf8"));
 	load_server_or_make_default(config.server_id, config.server_name);
+	wss = new WebSocket.Server({ port: config.port, noServer: true });
+
+	wss.on("connection", (ws, req) => {
+		console.log(`New connection from ${req.connection.remoteAddress}`);
+		await_handshake(ws).then((data) => {
+			console.log(`Handshake from ${data.id}`);
+			peer_online(data.id);
+			set_up_handlers(ws, data.id);
+		}).catch((err) => {
+			console.log(`Peer handshake error: ${err}`);
+			ws.close();
+		});
+	});
+
+	wss.on("error", (err) => {
+		console.log(`Error in connection: ${err}`);
+	});
+
+	// Connect to all peers in Orbit database
+	memory.cache.query((q) => q.findRecords("peer")).forEach((peer) => {
+		if (peer.id != this_server.id) {
+			connect_to_peer(peer);
+		}
+	});
 }
 
 module.exports = {
